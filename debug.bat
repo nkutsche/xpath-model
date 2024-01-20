24320c3560dec35ceace51cac8ce4741e2764d87
Nico Kutscherauer <kutscherauer@data2type.de>
2024-01-20 20:09:06 +0100

    Extends debug.bat

--- debug.bat
+++ debug.bat
@@ -1,1 +1,8 @@
-mvn exec:exec -Dexec.executable="java" -Dexec.args="%J_OPTS% -cp %%classpath net.sf.saxon.Transform -init:com.nkutsche.xslt.pkg.handler.PackageManager -config:src/test/xspec/qt3testsuite/saxon-config.xml -xsl:src/test/xsl/debugger.xsl -s:src/test/xsl/debugger.xsl -o:target/debug-result.xml"
+set xpath=%1
+
+if not "%xpath%"=="" set xpath=xpath=%xpath%
+
+mvn exec:exec -Dexec.executable="java" -Dexec.args="%J_OPTS% -cp %%classpath net.sf.saxon.Transform -init:com.nkutsche.xslt.pkg.handler.PackageManager -config:src/test/xspec/qt3testsuite/saxon-config.xml -xsl:src/test/xsl/debugger.xsl -s:src/test/xsl/debugger.xsl -o:target/debug-result.xml %xpath%"
+
+
+::mvn exec:exec -Dexec.executable="java" -Dexec.args="%J_OPTS% -cp %%classpath net.sf.saxon.Transform -config:src/test/xspec/qt3testsuite/saxon-config.xml -xsl:src/test/xsl/debugger2.xsl -s:src/test/xsl/debugger.xsl -o:target/debug-result.xml"
