045b7d25752b9a45e61c22bbcbbfd2f233c459ca
Nico Kutscherauer <kutscherauer@data2type.de>
2024-01-26 17:29:43 +0100

    Bugfix in debug.bat for quoting the parameter

--- debug.bat
+++ debug.bat
@@ -1,6 +1,6 @@
-set xpath=%1
+set xpath=%~1
 
-if not "%xpath%"=="" set xpath=xpath=%xpath%
+if not "%xpath%"=="" set xpath=xpath='%xpath%'
 
 mvn exec:exec -Dexec.executable="java" -Dexec.args="%J_OPTS% -cp %%classpath net.sf.saxon.Transform -init:com.nkutsche.xslt.pkg.handler.PackageManager -config:src/test/xspec/qt3testsuite/saxon-config.xml -xsl:src/test/xsl/debugger.xsl -s:src/test/xsl/debugger.xsl -o:target/debug-result.xml %xpath%"
 
